f5d2d60fce78c5dd94b4ec91cf275dd851cbc555
Nocye <zzh8626327@sina.com>
2021-01-08 17:36:30 +0800

    树的基本方法

--- Algorithm/Tree/Program.cs
+++ Algorithm/Tree/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace Tree
+﻿using System.Collections.Generic;
+
+namespace Tree
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
         }
+
+        #region 递归遍历
+
+        public void Preorder(TreeNode root, IList<int> temp)
+        {
+            if (root == null) return;
+
+            temp.Add(root.val);
+            Preorder(root.left, temp);
+            Preorder(root.right, temp);
+        }
+
+        public void Inorder(TreeNode root, IList<int> temp)
+        {
+            if (root == null) return;
+
+            Inorder(root.left, temp);
+            temp.Add(root.val);
+            Inorder(root.right, temp);
+        }
+
+        public void PostOrder(TreeNode root, IList<int> temp)
+        {
+            if (root == null) return;
+
+            PostOrder(root.left, temp);
+            PostOrder(root.right, temp);
+            temp.Add(root.val);
+        }
+
+        #endregion
     }
 }
---
+++ Algorithm/Tree/TreeNode.cs
@@ -0,0 +1,63 @@
+﻿//------------------------------------------------------------
+// 作者: Nocye
+// 日期: 2021年01月08日 星期五 17:07
+//------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Tree
+{
+    public class TreeNode
+    {
+        public TreeNode left;
+        public TreeNode right;
+        public int val;
+
+        public TreeNode(int value, TreeNode l = null, TreeNode r = null)
+        {
+            val = value;
+            left = l;
+            right = r;
+        }
+
+        /// <summary>
+        ///     根据一个数组创建一棵二叉树,同leetcode题目中的生成方式
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static TreeNode CreatBinaryTree(int[] arr)
+        {
+            var index = 0;
+            var root = new TreeNode(arr[index++]);
+            var queue = new Queue<TreeNode>(arr.Length);
+            queue.Enqueue(root);
+            TreeNode tempNode;
+            while (arr.Length > index)
+            {
+                tempNode = queue.Dequeue();
+                if (arr[index++] != -1 && tempNode != null)
+                {
+                    tempNode.left = new TreeNode(arr[index - 1]);
+                    queue.Enqueue(tempNode.left);
+                }
+                else
+                {
+                    queue.Enqueue(null);
+                }
+
+                if (arr.Length > index && arr[index++] != -1 && tempNode != null)
+                {
+                    tempNode.right = new TreeNode(arr[index - 1]);
+                    queue.Enqueue(tempNode.right);
+                }
+                else
+                {
+                    queue.Enqueue(null);
+                }
+            }
+
+            queue.Clear();
+            return root;
+        }
+    }
+}
